6762642eaa140357b2948ee715356e5d80fcefb3
prabin <prabin.171342@ncit.edu.np>
2022-06-09 17:12:20 +0545

    Components created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     CommonModule,
