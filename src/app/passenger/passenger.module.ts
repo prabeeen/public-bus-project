f01c3e7e5369c49d10c2e4f9510843dad055e1d6
prabin <prabin.171342@ncit.edu.np>
2022-07-29 16:10:05 +0545

    initial search component changes made

--- src/app/passenger/passenger.module.ts
+++ src/app/passenger/passenger.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { PassengerRoutingModule } from './passenger-routing.module';
-import { PassengerComponent } from './passenger.component';
 import { SharedModule } from '../shared/shared.module';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { PassengerRoutingModule } from './passenger-routing.module';
+
+import { PassengerComponent } from './passenger.component';
 import { TrackBusComponent } from './track-bus/track-bus.component';
-import { MatDividerModule } from '@angular/material/divider';
 import { SearchBusComponent } from './search-bus/search-bus.component';
 import { BusProgressComponent } from './bus-progress/bus-progress.component';
 import { PaymentComponent } from './payment/payment.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -25,10 +34,19 @@ import { PaymentComponent } from './payment/payment.component';
     CommonModule,
     PassengerRoutingModule,
     SharedModule,
+    ReactiveFormsModule,
+
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
     MatDividerModule,
+    MatAutocompleteModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+
   ]
 })
 export class PassengerModule { }
--- src/app/passenger/search-bus/search-bus.component.ts
+++ src/app/passenger/search-bus/search-bus.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { map, startWith} from 'rxjs/operators';
+
+interface busDetails {
+  title: string;
+  description: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-search-bus',
   templateUrl: './search-bus.component.html',
   styleUrls: ['./search-bus.component.scss']
 })
-export class SearchBusComponent implements OnInit {
+
+export class SearchBusComponent implements OnInit, OnDestroy {
+
+  routes:string[] = ['shantinagar','baneshwor','koteshwor','balkumari','gwarko',]
+  searchForm!: FormGroup;
+
+  filteredSourceRoutes$!: Observable<string[]>;
+  filteredSourceRoutes!: string[];
+  subSource!: Subscription;
+
+  filteredDestinationRoutes$!: Observable<string[]>;
+  filteredDestinationRoutes!: string[];
+  subDestination!: Subscription;
+
+  busDetail: busDetails[] = [
+    {title: "Nepal Yatayat", description: "Bus Info", route:"a, b, c, d, e, f"},
+    {title: "Mahanagar Yatayat", description: "Bus Info", route:"a, b, c, d, e, f"},
+    {title: "Mayur Yatayat", description: "Bus Info", route:"a, b, c, d, e, f"},
+    {title: "Ridhi Sidhi Yatayat", description: "Bus Info", route:"a, b, c, d, e, f"}
+  ]
+
+  toDisplayCard: boolean = false
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      sourceControl : new FormControl('',[Validators.required]),
+      destinationControl : new FormControl('',[Validators.required])
+    });
+
+    this.filteredSourceRoutes$ = this.searchForm.controls.sourceControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+    this.filteredDestinationRoutes$ = this.searchForm.controls.destinationControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+
+    this.subSource = this.filteredSourceRoutes$.subscribe(data=>{
+      this.filteredSourceRoutes = data
+    })
+
+    this.subDestination = this.filteredDestinationRoutes$.subscribe(data=>{
+      this.filteredDestinationRoutes = data
+    })
+
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.routes.filter(route => route.toLowerCase().startsWith(filterValue));
+  }
+
+  showCards(e:any){
+    this.toDisplayCard = true;
+  }
+
+  ngOnDestroy(): void {
+    if(this.subSource)
+    this.subSource.unsubscribe();
+    if(this.subDestination)
+    this.subDestination.unsubscribe();
   }
 
 }
+
